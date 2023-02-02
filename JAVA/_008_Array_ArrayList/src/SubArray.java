9d320c30762fbefb2b99400b9464c9aceaf84757
TheLastBaozi <xuvictorqi2003@gmail.com>
2023-02-02 22:23:47 +0100

    Added more thinks... right now it is the exercises

--- JAVA/_008_Array_ArrayList/src/SubArray.java
+++ JAVA/_008_Array_ArrayList/src/SubArray.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -8,8 +9,8 @@ public class SubArray {
         do {
             System.out.print("Tell me how many number do you want in the vector ( > 0 ):");
             toCheck = input.nextLine();
-            if (!isAnPositiveInteger(toCheck)) System.out.println("Not a valid number... please repeat");
-        } while (!isAnPositiveInteger(toCheck));
+            if (!isAnInteger(toCheck)) System.out.println("Not a valid number... please repeat");
+        } while (!isAnInteger(toCheck) || Integer.parseInt(toCheck) <=0);
 
         int[] numbaList = new int[Integer.parseInt(toCheck)];
         System.out.println("Vector Successfully created");
@@ -22,13 +23,37 @@ public class SubArray {
         do {
             System.out.print("Tell me the number you want to search :");
             toCheck = input.nextLine();
-        } while (!isAnPositiveInteger(toCheck));
+        } while (!isAnInteger(toCheck));
+        int toSearch = Integer.parseInt(toCheck);
+
+
+        // We want to traverse the entire Array
+        // Queremos recorrer el Array por completo
+        for (int listPosition = 0; listPosition < numbaList.length; listPosition++) {
+            // Temporally stored numbers
+            int temporal = 0;
+            // In reverse to position
+            for (int lastPosition = numbaList.length; lastPosition > listPosition; lastPosition--) {
+
+                for (int position = listPosition; position < lastPosition; position++) {
+                    // FIXME: COMPLETE HERE
+
+                    if (temporal == toSearch) {
+                        // FIXME: System Print here
+                        // Suggestion : FOR if u don't want store it in a String
+                    }
+                }
+            }
+        }
+
+
 
     }
 
-    private static boolean isAnPositiveInteger(String numbaToCheck) {
-        if (numbaToCheck.matches("\\d+")) {
+    private static boolean isAnInteger(String numbaToCheck) {
+        if (numbaToCheck.matches("-?\\d+")) {
             if (numbaToCheck.length() >= 10) {
+                // FIXME: Negative number didn't checked
                 return numbaToCheck.compareTo("2147483647") <= 0;
             } else {
                 return true;
